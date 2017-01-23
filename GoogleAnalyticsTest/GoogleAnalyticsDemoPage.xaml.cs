3ef2578f42fd9308225d05b4761481efac502a75
vaishali.limaye@gmail.com <vaishali.limaye@gmail.com>
2017-01-23 19:00:29 +0530

    initial commit

---
+++ GoogleAnalyticsTest/GoogleAnalyticsDemoPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace GoogleAnalyticsTest
+{
+    public partial class GoogleAnalyticsDemoPage : ContentPage
+    {
+        public GoogleAnalyticsDemoPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ GoogleAnalyticsTest/GoogleAnalyticsTestPage.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using Xamarin.Forms;
+
+namespace GoogleAnalyticsTest
+{
+    public partial class GoogleAnalyticsTestPage : ContentPage
+    {
+        private int counter = 0;
+
+        public GoogleAnalyticsTestPage()
+        {
+            if (Device.OS == TargetPlatform.Android)
+            {
+                //Track Page
+                GASCall.Track_App_Page("Android HomePage");
+            }
+            //or iOS
+            else {
+                //Track Page
+                GASCall.Track_App_Page("iOS HomePage");
+            }
+
+            InitializeComponent();
+        }
+
+        public void OnClickButtonGoogleEventMethod(object sender, EventArgs args)
+        {
+           
+            GoogleAnalyticsEvents(GAEventCategory.CountButton, "Button Pressed " + counter++);
+            //Navigate();
+
+        }
+        public void OnClickButtonExpGoogleEventMethod(object sender, EventArgs args)
+        {
+            
+            int i = 10;
+            var data = i / 0;
+        }
+
+        //sending event to google analytics
+        private void GoogleAnalyticsEvents(string Category, string EventToTrack)
+        {
+            GASCall.Track_App_Event(Category, EventToTrack);
+
+        }
+       
+
+
+    }
+}
